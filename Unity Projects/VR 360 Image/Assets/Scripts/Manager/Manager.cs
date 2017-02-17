0298140045e841fb1c3d387e4b436906c995f672
Robert Neijmeijer <robert98neijmeijer@gmail.com>
2017-02-17 12:01:24 +0100

    Some Updates

---
+++ Unity Projects/VR 360 Image/Assets/Scripts/Manager/Manager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Manager : MonoBehaviour {
+    public Texture[] images;
+    private int curImageNumber;
+    public GameObject projectionSphere;
+    public float clickCooldownTimer;
+    public bool canClick;
+
+    void Awake() {
+        canClick = true;
+    }
+
+    void Update() {
+        //Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved
+        if (canClick) {
+            //Input.touchCount > 0
+            if (Input.touchCount > 0) {
+                if (curImageNumber >= images.Length - 1) {
+                    curImageNumber = 0;
+                } else {
+                    curImageNumber++;
+                }
+                projectionSphere.GetComponent<Renderer>().material.mainTexture = images[curImageNumber];
+                canClick = false;
+                StartCoroutine("Cooldown");
+                print("Cooldown started");
+
+            }
+
+        }
+    }
+    IEnumerator Cooldown() {
+
+        yield return new WaitForSeconds(clickCooldownTimer);
+        canClick = true;
+        print("Cooldown stopped");
+        StopCoroutine("Cooldown");
+
+    }
+}
---
+++ Unity Projects/Velo-City project/Assets/Scripts/Robert/Interraction.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interraction : MonoBehaviour {
+    public enum TypeOffInterraction {NPC,Door}
+    public Door door;
+    public NPC npc;
+
+    public TypeOffInterraction typeOffObject;
+
+    public void Interract() {
+        switch (typeOffObject) {
+            case TypeOffInterraction.NPC:
+                npc.Interract();
+                break;
+            case TypeOffInterraction.Door:
+                door.DoorInterraction();
+                break;  
+        }
+    }
+}
+[System.Serializable]
+public class Door : Interraction  {
+    public Transform door1;
+    public Transform door2;
+    private bool isOpen;
+    public float openAngle;
+    public void DoorInterraction() {
+        if (isOpen) {
+            Close();
+            isOpen = false;
+        } else if (!isOpen) {
+            Open();
+            isOpen = true;
+        }
+    }
+
+    void Close() {
+        // play closing animation
+    }
+
+    void Open() {
+        // play opening animation
+    }
+}
+[System.Serializable]
+public class NPC : Interraction {
+    public Canvas canvas;
+    private bool canvasIsActive;
+    public string textToSay;
+    public Text text;
+    public void Interract() {
+        
+    }
+}
+
